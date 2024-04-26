03b651b5f2fbda89bcbe9899e87783bbaba60478
Antoine Bauchot <bchtantoine@gmail.com>
2024-04-26 14:49:22 +0200

    setup

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native-web';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+getAllPlanets = () => {
+  const url = 'https://graph.sprw.dev/api/graphql';
+  if(!url) {
+    console.log('Please provide the GRAPHQL_URL');
+    return Promise.reject(new Error('Please provide the GRAPHQL_URL'));
+  }
+  return axios.get(url, {
+    params: {
+      query: `
+        {
+          planets {
+            edges {
+              node {
+                name
+                id
+              }
+            }
+          }
+        }
+      `
+    }
+  });
+}
 
 export default function App() {
+  const [planets, setPlanets] = useState([]);
+  useEffect( () => {
+    getAllPlanets()
+    .then(response => {
+      setPlanets(response.data.data.planets.edges);
+    })
+    .catch(error => {
+      console.log('Error fecthing planets: ',error);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>List of planets:</Text>
+      {planets.map(planet => (
+        <Text key={planet.node.id}>{planet.node.name}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
